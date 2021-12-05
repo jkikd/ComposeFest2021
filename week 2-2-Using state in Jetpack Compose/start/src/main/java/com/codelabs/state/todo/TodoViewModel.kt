5bfdb997c4d796358c815177d47b223183c11ceb
jkikd <duswn0074@hanmail.net>
2021-12-05 17:40:22 +0900

    new upload 2021.12.05 - week2-2

--- week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoViewModel.kt
+++ week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoViewModel.kt
@@ -16,22 +16,53 @@
 
 package com.codelabs.state.todo
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class TodoViewModel : ViewModel() {
+    //state
+    //private var _todoItems = MutableLiveData(listOf<TodoItem>())
+    //val todoItems: LiveData<List<TodoItem>> = _todoItems
 
-    private var _todoItems = MutableLiveData(listOf<TodoItem>())
-    val todoItems: LiveData<List<TodoItem>> = _todoItems
+    //private state
+    private var currentEditPosition by mutableStateOf(-1)
 
+    //state - todoItems
+    var todoItems = mutableStateListOf<TodoItem>()
+    private set
+
+    //state
+    var currentEditItem:TodoItem?.get() = todoItems.getOrNull(currentEditPosition)
+    // event - 아이템 추가
     fun addItem(item: TodoItem) {
-        _todoItems.value = _todoItems.value!! + listOf(item)
+        //_todoItems.value = _todoItems.value!! + listOf(item)
+        todoItems.add(item)
+    }
+    //event - 아이템 제거
+    fun removeItem(item: TodoItem) {
+        /*_todoItems.value = _todoItems.value!!.toMutableList().also {
+            it.remove(item)
+        }*/
+        todoItems.remove(item)
+        onEditDone()//항목제거시 종료
     }
 
-    fun removeItem(item: TodoItem) {
-        _todoItems.value = _todoItems.value!!.toMutableList().also {
-            it.remove(item)
+    //event - onEditDone
+    fun onEditDone(){
+        currentEditPosition = -1
+    }
+
+    //event - onEditItemChange
+    fun onEditItemChange(item: TodoItem){
+        val currentItem = requireNotNull(currentEditItem)
+        require(currentItem.id==item.id){
+            "You can only change an item with the same id as currentEditItem"
         }
+        todoItems[currentEditPosition] = item
     }
 }
